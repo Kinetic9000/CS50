bfda57f16e019d197821901727828f2007add748
Kinetic9000 <113357834+Kinetic9000@users.noreply.github.com>
2024-06-24 11:06:01 +0530

    ....

---
+++ Codes/plates.py
@@ -0,0 +1,21 @@
+
+            
+           
+def main():
+    plate = input("Enter a vanity plate: ").strip().upper()
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    
+ if 2 <= len(s) <= 6 and  s.isalnum() and s[0:2].isalpha():
+    for i in range(len(s)):
+        if s[i].isdigit():
+            if s[i] == "0":
+                return False
+            return s[i:].isdigit()
+    return True
+
+main()
