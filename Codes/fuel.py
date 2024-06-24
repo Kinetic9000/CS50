2f4f339b0c336f6f089736875672e1ebe9b15377
Kinetic9000 <113357834+Kinetic9000@users.noreply.github.com>
2024-06-24 19:26:37 +0530

    ....

---
+++ Codes/fuel.py
@@ -0,0 +1,30 @@
+def fuel(fraction):
+    try:
+        x, y = fraction.split('/')
+        x = int(x)
+        y = int(y)
+        if x > y or y == 0:
+            raise ValueError
+        percentage = (x / y) * 100
+        return round(percentage)
+    except (ValueError, ZeroDivisionError):
+        return None
+
+def main():
+    while True:
+        fraction = input("Fraction: ")
+        percentage = fuel(fraction)
+        if percentage is not None:
+            if percentage <= 1:
+                print("E")
+            elif percentage >= 99:
+                print("F")
+            else:
+                print(f"{percentage}%")
+            break
+        else:
+           continue
+
+main()
+
+        
