f1a0954ec45285b272ac47728a0f32077cc9df0d
Kinetic9000 <113357834+Kinetic9000@users.noreply.github.com>
2024-06-26 21:24:00 +0530

    ....

--- Codes/deep.py
+++ Codes/deep.py
@@ -1,10 +1,14 @@
-x=input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower()
-match x:
-    case "42":
-        print("Yes")
-    case "forty-two":
-        print("Yes")
-    case "forty two":
-        print("Yes")
-    case _:
-        print("No")
+def main():
+    x=input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower()
+    match x:
+        case "42":
+            print("Yes")
+        case "forty-two":
+            print("Yes")
+        case "forty two":
+            print("Yes")
+        case _:
+            print("No")
+
+if __name__ == "__main__":
+    main()
